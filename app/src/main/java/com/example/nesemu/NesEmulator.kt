700dcb0c114662b2da3fc9d27249ae2ed0bc36ba
Zash60 <xath06@gmail.com>
2025-12-04 10:12:09 -0300

    Update NesEmulator.kt

--- app/src/main/java/com/example/nesemu/NesEmulator.kt
+++ app/src/main/java/com/example/nesemu/NesEmulator.kt
@@ -1,9 +1,17 @@
 package com.example.nesemu
+
 import android.graphics.Bitmap
+
 class NesEmulator {
-    companion object { init { System.loadLibrary("nes-emu") } }
+    companion object {
+        init {
+            System.loadLibrary("nes-emu")
+        }
+    }
+
     external fun init()
-    external fun loadRom(path: String)
+    // AGORA RECEBE BYTE ARRAY
+    external fun loadRom(romData: ByteArray)
     external fun runFrame(bitmap: Bitmap, rewind: Boolean)
     external fun setButtonState(buttonId: Int, pressed: Boolean)
     external fun saveState(save: Boolean)
