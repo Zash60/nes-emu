33f9269d6b0a618d9d2423c58738a68b7c8f60d5
Zash60 <xath06@gmail.com>
2025-12-04 09:28:35 -0300

    Update SDL.h

--- app/src/main/cpp/include/SDL.h
+++ app/src/main/cpp/include/SDL.h
@@ -2,11 +2,11 @@
 #include "SDL_scancode.h"
 #include "SDL_render.h"
 
-// Definições básicas para evitar erros
+// Tipos básicos
 typedef unsigned char Uint8;
 typedef unsigned int Uint32;
 
-// Stub para funções de áudio do SDL se aparecerem
+// Áudio Dummy
 typedef uint16_t SDL_AudioFormat;
 #define AUDIO_S16 0x8010
 typedef void (*SDL_AudioCallback)(void *userdata, Uint8 * stream, int len);
@@ -19,15 +19,19 @@ typedef struct SDL_AudioSpec {
     void *userdata;
 } SDL_AudioSpec;
 
-inline void SDL_InitSubSystem(unsigned int) {}
-inline void SDL_QuitSubSystem(unsigned int) {}
-inline int SDL_OpenAudioDevice(const char*, int, const SDL_AudioSpec*, void*, int) { return 1; }
-inline void SDL_CloseAudioDevice(int) {}
-inline void SDL_PauseAudioDevice(int, int) {}
-inline void SDL_LockAudioDevice(int) {}
-inline void SDL_UnlockAudioDevice(int) {}
-inline void SDL_zero(SDL_AudioSpec) {}
+// Funções Gerais Dummy
+inline void SDL_InitSubSystem(unsigned int flags) {}
+inline void SDL_QuitSubSystem(unsigned int flags) {}
+inline int SDL_OpenAudioDevice(const char* device, int iscapture, const SDL_AudioSpec* desired, void* obtained, int allowed_changes) { return 1; }
+inline void SDL_CloseAudioDevice(int dev) {}
+inline void SDL_PauseAudioDevice(int dev, int pause_on) {}
+inline void SDL_LockAudioDevice(int dev) {}
+inline void SDL_UnlockAudioDevice(int dev) {}
+inline void SDL_zero(SDL_AudioSpec x) {}
 inline const char* SDL_GetError() { return ""; }
+
 #define SDL_INIT_AUDIO 0x00000010
 #define SDL_AUDIO_ALLOW_ANY_CHANGE 0
 typedef int SDL_AudioDeviceID;
+
+#define SDL_MAIN_HANDLED
