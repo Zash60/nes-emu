4bcb28d30b25a2002313442d10cf9971cf0e9862
Zash60 <xath06@gmail.com>
2025-12-04 09:36:45 -0300

    Update System.cpp

--- app/src/main/cpp/core/System.cpp
+++ app/src/main/cpp/core/System.cpp
@@ -2,146 +2,160 @@
 #include "IO.h"
 #include <SDL.h>
 #include <chrono>
+#include <cstring> // Necessário para strlen
 
 namespace System
 {
-	const char* GetAppDirectory()
-	{
-		static char appDir[2048] = { 0 };
-		
-		// Lazily build the app directory
-		if (appDir[0] == 0)
-		{
-			std::string temp = SDL_GetBasePath();
+    const char* GetAppDirectory()
+    {
+        static char appDir[2048] = { 0 };
+        
+        // Lazily build the app directory
+        if (appDir[0] == 0)
+        {
+            // No Android com nosso Fake SDL, isso retorna string vazia
+            char* basePath = SDL_GetBasePath();
+            std::string temp = basePath ? basePath : "";
+            if (basePath) SDL_free(basePath);
 
-			// Find the app name directory in the path and return full path to that directory.
-			// Mostly useful when running out of Debug/Release during development.
-			auto npos = temp.find(APP_NAME);
-			if (npos != std::string::npos)
-			{
-				temp.copy(appDir, npos + strlen(APP_NAME) + 1, 0);
-			}
-			else
-			{
-				temp.copy(appDir, temp.length(), 0);
-			}
+            // Se vazio (Android fake), usa um caminho seguro padrão ou vazio
+            if (temp.empty()) {
+                // No Android, caminhos são gerenciados pelo Java/Context, 
+                // mas para evitar crash no C++, deixamos vazio ou "."
+                temp = "."; 
+            }
 
-			temp = appDir; // Just for asserting
-			assert(temp.size() > 0 && temp.size() < ARRAYSIZE(appDir));
-			assert(temp.back() == '\\' || temp.back() == '/');
-		}
+            auto npos = temp.find(APP_NAME);
+            if (npos != std::string::npos)
+            {
+                temp.copy(appDir, npos + strlen(APP_NAME) + 1, 0);
+            }
+            else
+            {
+                // Garante que cabe no buffer
+                size_t len = temp.length();
+                if (len >= sizeof(appDir)) len = sizeof(appDir) - 1;
+                temp.copy(appDir, len, 0);
+                appDir[len] = '\0';
+            }
+        }
 
-		return appDir;
-	}
+        return appDir;
+    }
 
-	void Sleep(uint32 ms)
-	{
-		SDL_Delay(ms);
-	}
+    void Sleep(uint32 ms)
+    {
+        SDL_Delay(ms);
+    }
 
-	float64 GetTimeSec()
-	{
-		static Uint64 start = SDL_GetPerformanceCounter();
-		return static_cast<float64>(SDL_GetPerformanceCounter() - start) / SDL_GetPerformanceFrequency();
-	}
+    float64 GetTimeSec()
+    {
+        static Uint64 start = SDL_GetPerformanceCounter();
+        return static_cast<float64>(SDL_GetPerformanceCounter() - start) / SDL_GetPerformanceFrequency();
+    }
 }
 
-
+// Implementações específicas de plataforma
 
 #if PLATFORM_WINDOWS
 
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
-#undef ARRAYSIZE // Already defined in a Windows header
+#undef ARRAYSIZE 
 #include <Windows.h>
 #include <commdlg.h>
 #include <conio.h>
 #include <cstdio>
 
-// Undef the macros in WinUser.h so we can use these names
 #undef MessageBox
 #undef CreateDirectory
 
 namespace System
 {
-	bool CreateDirectory(const char* directory)
-	{
-		return ::CreateDirectoryA(directory, NULL) != FALSE;
-	}
+    bool CreateDirectory(const char* directory)
+    {
+        return ::CreateDirectoryA(directory, NULL) != FALSE;
+    }
 
-	void DebugBreak()
-	{
-		::DebugBreak();
-	}
+    void DebugBreak()
+    {
+        ::DebugBreak();
+    }
 
-	void MessageBox(const char* title, const char* message)
-	{
-		printf(FormattedString<>("%s: %s\n", title, message).Value());
-		::MessageBoxA(::GetActiveWindow(), message, title, MB_OK);
-	}
-	
-	bool SupportsOpenFileDialog()
-	{
-		return true;
-	}
+    void MessageBox(const char* title, const char* message)
+    {
+        printf("%s: %s\n", title, message);
+        ::MessageBoxA(::GetActiveWindow(), message, title, MB_OK);
+    }
+    
+    bool SupportsOpenFileDialog()
+    {
+        return true;
+    }
 
-	bool OpenFileDialog(std::string& fileSelected, const char* title, const char* filter)
-	{
-		char file[_MAX_PATH] = "";
+    bool OpenFileDialog(std::string& fileSelected, const char* title, const char* filter)
+    {
+        char file[_MAX_PATH] = "";
+        char currDir[_MAX_PATH] = "";
+        ::GetCurrentDirectoryA(sizeof(currDir), currDir);
 
-		char currDir[_MAX_PATH] = "";
-		::GetCurrentDirectoryA(sizeof(currDir), currDir);
+        OPENFILENAMEA ofn = {0};
+        ofn.lStructSize = sizeof(ofn);
+        ofn.lpstrFile = file;
+        ofn.nMaxFile = sizeof(file);
+        ofn.lpstrTitle = title;
+        ofn.lpstrFilter = filter;
+        ofn.nFilterIndex = 0;
+        ofn.lpstrInitialDir = currDir;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 
-		OPENFILENAMEA ofn = {0};
-		ofn.lStructSize = sizeof(ofn);
-		ofn.lpstrFile = file;
-		ofn.nMaxFile = sizeof(file);
-		ofn.lpstrTitle = title;
-		ofn.lpstrFilter = filter;
-		ofn.nFilterIndex = 0;
-		ofn.lpstrInitialDir = currDir;
-		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-
-		if (::GetOpenFileNameA(&ofn) == TRUE)
-		{
-			fileSelected = ofn.lpstrFile;
-			return true;
-		}
-		return false;
-	}
+        if (::GetOpenFileNameA(&ofn) == TRUE)
+        {
+            fileSelected = ofn.lpstrFile;
+            return true;
+        }
+        return false;
+    }
 }
 
-#elif PLATFORM_LINUX || PLATFORM_MAC
+// CORREÇÃO: Removido caractere unicode invisível na linha abaixo
+#elif defined(__ANDROID__) || PLATFORM_LINUX || PLATFORM_MAC
 
 #include <sys/stat.h>
+#include <unistd.h>
+#include <cassert>
 
 namespace System
 {
-	bool CreateDirectory(const char* directory)
-	{
-		return mkdir(directory, 0777) == 0;
-	}
+    bool CreateDirectory(const char* directory)
+    {
+        struct stat st = {0};
+        if (stat(directory, &st) == -1) {
+            return mkdir(directory, 0777) == 0;
+        }
+        return false;
+    }
 
-	void DebugBreak()
-	{
-		assert(false); //@TODO: better way to do this?
-	}
+    void DebugBreak()
+    {
+        // No Android/Linux, assert(false) ou raise(SIGTRAP)
+        // assert(false); 
+    }
 
-	void MessageBox(const char* title, const char* message)
-	{
-		printf("%s: %s\n", title, message);
-	}
-	
-	bool SupportsOpenFileDialog()
-	{
-		return false;
-	}
+    void MessageBox(const char* title, const char* message)
+    {
+        printf("%s: %s\n", title, message);
+    }
+    
+    bool SupportsOpenFileDialog()
+    {
+        return false;
+    }
 
-	bool OpenFileDialog(std::string& fileSelected, const char* title, const char* filter)
-	{
-		assert(SupportsOpenFileDialog());
-		return false;
-	}
+    bool OpenFileDialog(std::string& fileSelected, const char* title, const char* filter)
+    {
+        return false;
+    }
 }
 
 #else
